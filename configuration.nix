70fae666370cd96dddc6298e440216f4b8418719
Benjamin <bwood3698@gmail.com>
2024-03-15 20:49:50 -0400

    Added appimage support
    Added appimagekit and appimage-run to SYSTEM PACKAGES
    
    Added automatic recognition and execution of appimages

--- configuration.nix
+++ configuration.nix
@@ -29,6 +29,8 @@
     piper
      libratbag
     git
+    appimagekit
+    appimage-run
   ];
 
   ### ENABLE PROGRAMS
@@ -98,6 +100,15 @@
     pulse.enable = true;
   };
 
+  # Recognize and Run AppImages
+  boot.binfmt.registrations.appimage = {
+  wrapInterpreterInShell = false;
+  interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+  recognitionType = "magic";
+  offset = 0;
+  mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+  magicOrExtension = ''\x7fELF....AI\x02'';
+};
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
