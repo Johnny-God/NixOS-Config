1d8b0519309ab11fd265cd6f4cc44e6bca4c0bcf
Benjamin <bwood3698@gmail.com>
2024-03-15 21:22:49 -0400

    Added flakes support

--- configuration.nix
+++ configuration.nix
@@ -45,6 +45,10 @@
     # Linux kernel
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
+    # Enable Nix Flakes
+  nix.extraOptions = ''experimental-features = nix-command flakes'';
+
+
     # Recognize and Run AppImages
   boot.binfmt.registrations.appimage = {
   wrapInterpreterInShell = false;
@@ -54,10 +58,6 @@
   mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
   magicOrExtension = ''\x7fELF....AI\x02'';
 };
-
-  # Enable Nix Flakes
-  nix.extraOptions = ''experimental-features = nix-command flakes'';
-
 
 ################################################################################################################################################# DEFAULT SYSTEM CONFIGURATION
 
